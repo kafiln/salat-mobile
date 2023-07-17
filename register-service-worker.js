9563fa96b148c35e23fe9e984329755e2817dbf5
Kafil NASDAMI <nasdami.kafil@bcg.com>
2023-07-17 03:26:43 +0100

    Updates

--- register-service-worker.js
+++
@@ -1,1 +0,0 @@
-"serviceWorker"in navigator&&window.addEventListener("load",(function(){navigator.serviceWorker.register("/salat-mobile/expo-service-worker.js",{scope:"/salat-mobile/"}).then((function(e){})).catch((function(e){console.info("Failed to register service-worker",e)}))}));
